99531e57374d06f059af03fc0d96dc3cb996b1e8
Mariusz Antonik <mantonik@gmail.com>
2022-03-19 15:15:25 -0400

    Update

--- server-config/bin/get_lets_encrypt_ssl.sh
+++ server-config/bin/get_lets_encrypt_ssl.sh
@@ -1,13 +1,32 @@
 #!/bin/bash 
 
-DOMAIN=$1
+if [ $1"x" == "x" ]; then 
+  echo "Enter domain name:"
+  read DOMAIN 
+else
+  DOMAIN=$1
+fi
 
+echo "---------------------------"
 #get root dir from configuration file 
-cat /etc/nginx/conf.d/$1.conf|grep "root"|grep "/data/www"|grep -v "#"
-
-
 ROOT_DIR=`/etc/nginx/conf.d/${DOMAIN}.conf|grep "root"|grep "/data/www"|grep -v "#"|sed -s " "|cut -d " " -f 4|sed 's|;||'`
 
+if [ ${ROOT_DIR}"x" == "x" ]; then 
+  echo "ROOT directory is empty - exit"
+fi
+
+if [ ! -e ${ROOT_DIR} ]; then 
+  echo "--------------------------"
+  echo "ROOT_DIR " ${ROOT_DIR}
+  echo "doesn't exist - exit"
+  echo "--------------------------"
+fi
+
+
+echo "Generate Let's encrytp SSL certificate for"
+echo "Domain: "${DOMAIN}
+echo "ROOT_DIR: " ${ROOT_DIR}
+echo "---------------------------"
 certbot --webroot -w ${ROOT_DIR}/htdocs  -d ${DOMAIN} -d www.${DOMAIN}
 
 service nginx restart
